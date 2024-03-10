7fddc25c7c8d3653d2543862cba7eb0aef2bea4b
Mykhailo Kushchov <mkuschov@gmail.com>
2024-03-09 22:04:07 -0500

    add pages & transition

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+	return (
+		<section>
+			<h1 className="text-7xl font-extrabold text-center mt-10">About</h1>
+		</section>
+	);
+};
+
+export default AboutPage;
---
+++ src/app/work/page.tsx
@@ -0,0 +1,9 @@
+const WorkPage = () => {
+	return (
+		<section>
+			<h1 className="text-7xl font-extrabold text-center mt-10">Work</h1>
+		</section>
+	);
+};
+
+export default WorkPage;
